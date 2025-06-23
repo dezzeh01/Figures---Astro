9d38c91f8cf27211ef1fe0ac9b88c123b254d6c0
chris <chris@dezzeh.com>
2025-06-23 16:54:08 +0100

    amended config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import astroIcon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({

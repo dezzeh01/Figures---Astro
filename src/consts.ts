1b09073240c3142e3d64a503ec97a9d29fc52575
chris <chris@dezzeh.com>
2025-06-23 16:39:27 +0100

    Added images

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dezzehs Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dezzehs Action Figure Stuff';
+export const SITE_DESCRIPTION = 'Welcome to my little corner of 80s nostalgia.  Here you will fine a lot of posts about Transformers, and then some other stuff as well.';
